bb1c5a8562eff46d0d86435c91c125b0dca5759d
Priyanka Solase <solasepriyanka83@gmail.com>
2026-07-10 13:56:43 +0530

    Create LargestNumber.java

---
+++ LargestNumber.java
@@ -0,0 +1,9 @@
+public class LargestNumber {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        if(a > b)
+            System.out.println(a + " is largest");
+        else
+            System.out.println(b + " is largest");
+    }
+}

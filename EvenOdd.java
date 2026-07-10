79c96e5baa8b0e0c1abf93c7d8023664b229ca5a
Priyanka Solase <solasepriyanka83@gmail.com>
2026-07-10 13:55:40 +0530

    Create  EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,9 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 8;
+        if(num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
